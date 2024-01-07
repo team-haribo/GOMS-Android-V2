a18341d4657ade0fffab95a552c3d58cd14616bb
GSM-Minseok <ms0818000@gmail.com>
2024-01-07 23:24:55 +0900

    :tada: :: init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GOMS-Android-V2"
+include(":app")
+ 
