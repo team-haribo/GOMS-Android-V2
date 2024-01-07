0b82be384325713a09f4f2a44c7d478122851664
GSM-Minseok <ms0818000@gmail.com>
2024-01-07 23:46:16 +0900

    :page_facing_up: :: Change compileSdk and targetSdk version 33 -> 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.goms.goms_android_v2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.goms.goms_android_v2"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
