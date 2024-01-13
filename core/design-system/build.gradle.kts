4a6085f5b3969c7037c5b06806b2eab640821a69
송현서 <s22026@gsm.hs.kr>
2024-01-13 19:39:59 +0900

    :heavy_plus_sign: :: Add GAuth dependency

--- core/design-system/build.gradle.kts
+++ core/design-system/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("goms.android.core")
@@ -6,9 +9,26 @@ plugins {
 }
 
 android {
+    buildFeatures {
+        buildConfig = true
+    }
+
+    defaultConfig {
+        buildConfigField("String", "CLIENT_ID",  getApiKey("CLIENT_ID"))
+        buildConfigField("String", "REDIRECT_URL",  getApiKey("REDIRECT_URL"))
+    }
+
     namespace = "com.goms.design_system"
 }
 
 dependencies {
     implementation(libs.coil.kt.compose)
+    implementation(libs.gauth)
+}
+
+fun getApiKey(propertyKey: String): String {
+    val propFile = rootProject.file("./local.properties")
+    val properties = Properties()
+    properties.load(FileInputStream(propFile))
+    return properties.getProperty(propertyKey)
 }
