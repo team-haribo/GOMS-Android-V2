1d28ded029d3c0d0cc0366b65e1833ecd8cfdb27
송현서 <s22026@gsm.hs.kr>
2024-01-13 19:40:32 +0900

    :heavy_plus_sign: :: Add gomsClickable

---
+++ core/design-system/src/main/java/com/goms/design_system/component/modifier/MultipleEventsCutter.kt
@@ -0,0 +1,24 @@
+package com.goms.design_system.component.modifier
+
+internal interface MultipleEventsCutter {
+    fun processEvent(event: () -> Unit)
+
+    companion object
+}
+
+internal fun MultipleEventsCutter.Companion.get(): MultipleEventsCutter =
+    MultipleEventsCutterImpl()
+
+private class MultipleEventsCutterImpl : MultipleEventsCutter {
+    private val now: Long
+        get() = System.currentTimeMillis()
+
+    private var lastEventTimeMs: Long = 0
+
+    override fun processEvent(event: () -> Unit) {
+        if (now - lastEventTimeMs >= 1000L) {
+            event.invoke()
+        }
+        lastEventTimeMs = now
+    }
+}
---
+++ core/design-system/src/main/java/com/goms/design_system/component/modifier/gomsClickable.kt
@@ -0,0 +1,36 @@
+package com.goms.design_system.component.modifier
+
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.semantics.Role
+
+fun Modifier.gomsClickable(
+    enabled: Boolean = true,
+    isIndication: Boolean = false,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    onClick: () -> Unit
+) = composed(
+    inspectorInfo = debugInspectorInfo {
+        name = "clickable"
+        properties["enabled"] = enabled
+        properties["onClickLabel"] = onClickLabel
+        properties["role"] = role
+        properties["onClick"] = onClick
+    }
+) {
+    val multipleEventsCutter = remember { MultipleEventsCutter.get() }
+    Modifier.clickable(
+        enabled = enabled,
+        onClickLabel = onClickLabel,
+        onClick = { multipleEventsCutter.processEvent { onClick() } },
+        role = role,
+        indication = if (isIndication) LocalIndication.current else null,
+        interactionSource = remember { MutableInteractionSource() }
+    )
+}
