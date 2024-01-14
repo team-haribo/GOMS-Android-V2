d2265193b3ead5489128bfc02f9640d477ae67e1
송현서 <s22026@gsm.hs.kr>
2024-01-14 20:18:16 +0900

    :lipstick: :: Add email login navigation

--- app/src/main/java/com/goms/goms_android_v2/navigation/GomsNavHost.kt
+++ app/src/main/java/com/goms/goms_android_v2/navigation/GomsNavHost.kt
@@ -4,8 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import com.goms.goms_android_v2.ui.GomsAppState
+import com.goms.login.navigation.emailLoginScreen
 import com.goms.login.navigation.loginRoute
 import com.goms.login.navigation.loginScreen
+import com.goms.login.navigation.navigateToEmailLogin
 import com.goms.login.navigation.navigateToLogin
 
 @Composable
@@ -21,7 +23,11 @@ fun GomsNavHost(
         modifier = modifier
     ) {
         loginScreen(
-            onNumberLoginClick = navController::navigateToLogin // number login 나오면 수정
+            onEmailLoginClick = navController::navigateToEmailLogin
+        )
+        emailLoginScreen(
+            onLoginClick = navController::navigateToLogin,
+            onNumberLoginClick = navController::navigateToLogin
         )
     }
 }
--- feature/login/src/main/java/com/goms/login/navigation/LoginNavigation.kt
+++ feature/login/src/main/java/com/goms/login/navigation/LoginNavigation.kt
@@ -4,17 +4,35 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.goms.login.EmailRoute
 import com.goms.login.LoginRoute
 
 const val loginRoute = "login_route"
+const val emailLoginRoute = "email_login_Route"
 
 fun NavController.navigateToLogin(navOptions: NavOptions? = null) {
     this.navigate(loginRoute, navOptions)
 }
 
-fun NavGraphBuilder.loginScreen(onNumberLoginClick: () -> Unit) {
+fun NavGraphBuilder.loginScreen(onEmailLoginClick: () -> Unit) {
     composable(route = loginRoute) {
         LoginRoute(
+            onEmailLoginClick = onEmailLoginClick
+        )
+    }
+}
+
+fun NavController.navigateToEmailLogin(navOptions: NavOptions? = null) {
+    this.navigate(emailLoginRoute, navOptions)
+}
+
+fun NavGraphBuilder.emailLoginScreen(
+    onLoginClick: () -> Unit,
+    onNumberLoginClick: () -> Unit
+) {
+    composable(route = emailLoginRoute) {
+        EmailRoute(
+            onLoginClick = onLoginClick,
             onNumberLoginClick = onNumberLoginClick
         )
     }
