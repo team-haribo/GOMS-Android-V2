b85b97aeafa248419f82b9e5c2c10abe5abebadc
GSM-Minseok <ms0818000@gmail.com>
2024-01-15 18:02:30 +0900

    :memo: :: Add datastore-proto

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -10,6 +10,7 @@ android {
 
 dependencies {
     implementation(project(":core:model"))
+    implementation(project(":core:datastore-proto"))
 
     implementation(libs.androidx.dataStore.core)
     implementation(libs.protobuf.kotlin.lite)
