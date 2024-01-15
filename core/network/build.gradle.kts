6bb9537445a4c7c849a73de05cd963370985f5c5
GSM-Minseok <ms0818000@gmail.com>
2024-01-15 17:37:42 +0900

    :memo: :: Change plugins

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,7 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
+    id("goms.android.core")
+    id("goms.android.hilt")
 }
 
 android {
