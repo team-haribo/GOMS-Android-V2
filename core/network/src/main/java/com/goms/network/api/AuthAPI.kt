9f9364c2c5d52e1fc040219fde9c519ba410d75a
송현서 <s22026@gsm.hs.kr>
2024-01-15 23:51:22 +0900

    :bug: :: Fix url

--- core/network/src/main/java/com/goms/network/api/AuthAPI.kt
+++ core/network/src/main/java/com/goms/network/api/AuthAPI.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthAPI {
-    @POST("signin")
+    @POST("/api/v2/auth/signin")
     suspend fun login(
         @Body body: LoginRequest
     ): LoginResponse
