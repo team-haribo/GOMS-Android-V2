5dbbad96fba2d87514793ea935af79ff42b63ea6
송현서 <s22026@gsm.hs.kr>
2024-01-17 15:23:26 +0900

    :sparkles: :: Change file to use Result type with Flow

--- core/domain/src/main/java/com/goms/domain/auth/LoginUseCase.kt
+++ core/domain/src/main/java/com/goms/domain/auth/LoginUseCase.kt
@@ -2,12 +2,13 @@ package com.goms.domain.auth
 
 import com.goms.data.repository.auth.AuthRepository
 import com.goms.model.request.auth.LoginRequest
+import com.goms.model.response.auth.LoginResponse
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
-    suspend operator fun invoke(body: LoginRequest) = kotlin.runCatching {
+    suspend operator fun invoke(body: LoginRequest): Flow<LoginResponse> =
         authRepository.login(body = body)
-    }
 }
