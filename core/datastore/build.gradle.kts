a5c2cc9de7574afc645073ba5f691655b8618a84
송현서 <s22026@gsm.hs.kr>
2024-01-17 15:17:30 +0900

    :sparkles: :: Add auth token proto file

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -2,16 +2,34 @@
 plugins {
     id("goms.android.core")
     id("goms.android.hilt")
+    alias(libs.plugins.protobuf)
 }
 
 android {
     namespace = "com.goms.datastore"
 }
 
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                register("java") {
+                    option("lite")
+                }
+                register("kotlin") {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
+
 dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:model"))
-    implementation(project(":core:datastore-proto"))
 
     implementation(libs.androidx.dataStore.core)
     implementation(libs.protobuf.kotlin.lite)
