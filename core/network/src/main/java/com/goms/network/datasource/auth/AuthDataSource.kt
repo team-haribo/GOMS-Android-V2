ccbcb58302e4975ac219a805a6726a0e88ca7fe4
송현서 <s22026@gsm.hs.kr>
2024-01-23 17:51:59 +0900

    :sparkles: :: Add sign up to AuthDataSource

--- core/network/src/main/java/com/goms/network/datasource/auth/AuthDataSource.kt
+++ core/network/src/main/java/com/goms/network/datasource/auth/AuthDataSource.kt
@@ -1,9 +1,17 @@
 package com.goms.network.datasource.auth
 
 import com.goms.model.request.auth.LoginRequest
+import com.goms.model.request.auth.SendNumberRequest
+import com.goms.model.request.auth.SignUpRequest
 import com.goms.model.response.auth.LoginResponse
 import kotlinx.coroutines.flow.Flow
 
 interface AuthDataSource {
+    suspend fun signUp(body: SignUpRequest): Flow<Unit>
+
     suspend fun login(body: LoginRequest): Flow<LoginResponse>
+
+    suspend fun sendNumber(body: SendNumberRequest): Flow<Unit>
+
+    suspend fun verifyNumber(email: String, authCode: String): Flow<Unit>
 }
