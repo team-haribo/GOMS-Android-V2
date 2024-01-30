bde55e16f83ea1d9e3d2fea787abf9fb00afa280
송현서 <s22026@gsm.hs.kr>
2024-01-30 22:12:17 +0900

    :bug: :: Fix uuid -> string

--- core/model/src/main/java/com/goms/model/response/outing/OutingResponse.kt
+++ core/model/src/main/java/com/goms/model/response/outing/OutingResponse.kt
@@ -4,11 +4,10 @@ import com.goms.model.enum.Gender
 import com.goms.model.enum.Major
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import java.util.UUID
 
 @JsonClass(generateAdapter = true)
 data class OutingResponse(
-    @Json(name = "accountIdx") val accountIdx: UUID,
+    @Json(name = "accountIdx") val accountIdx: String,
     @Json(name = "name") val name: String,
     @Json(name = "major") val major: Major,
     @Json(name = "grade") val grade: Int,
