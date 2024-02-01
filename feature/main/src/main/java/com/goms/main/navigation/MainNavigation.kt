4cc028d24e87b186612fd57a7d3db1f006af3d87
송현서 <s22026@gsm.hs.kr>
2024-02-01 20:49:27 +0900

    :sparkles: :: Connect outing status navigation

--- feature/main/src/main/java/com/goms/main/navigation/MainNavigation.kt
+++ feature/main/src/main/java/com/goms/main/navigation/MainNavigation.kt
@@ -1,19 +1,44 @@
 package com.goms.main.navigation
 
+import androidx.lifecycle.ViewModelStoreOwner
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.goms.main.MainRoute
+import com.goms.main.OutingStatusRoute
 
 const val mainRoute = "main_route"
+const val outingStatusRoute = "outing_status_route"
 
 fun NavController.navigateToMain(navOptions: NavOptions? = null) {
     this.navigate(mainRoute, navOptions)
 }
 
-fun NavGraphBuilder.mainScreen() {
+fun NavGraphBuilder.mainScreen(
+    viewModelStoreOwner: ViewModelStoreOwner,
+    onOutingStatusClick: () -> Unit
+) {
     composable(route = mainRoute) {
-        MainRoute()
+        MainRoute(
+            viewModelStoreOwner = viewModelStoreOwner,
+            onOutingStatusClick = onOutingStatusClick
+        )
+    }
+}
+
+fun NavController.navigateToOutingStatus(navOptions: NavOptions? = null) {
+    this.navigate(outingStatusRoute, navOptions)
+}
+
+fun NavGraphBuilder.outingStatusScreen(
+    viewModelStoreOwner: ViewModelStoreOwner,
+    onBackClick: () -> Unit
+) {
+    composable(route = outingStatusRoute) {
+        OutingStatusRoute(
+            viewModelStoreOwner = viewModelStoreOwner,
+            onBackClick = onBackClick
+        )
     }
 }
