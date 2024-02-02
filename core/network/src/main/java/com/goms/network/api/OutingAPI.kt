41abfe39e48ad6a3f309c836c7267a16daf9bc52
송현서 <s22026@gsm.hs.kr>
2024-02-02 18:12:19 +0900

    :sparkles: :: Add outing search to OutingAPI

--- core/network/src/main/java/com/goms/network/api/OutingAPI.kt
+++ core/network/src/main/java/com/goms/network/api/OutingAPI.kt
@@ -3,6 +3,7 @@ package com.goms.network.api
 import com.goms.model.response.outing.CountResponse
 import com.goms.model.response.outing.OutingResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface OutingAPI {
     @GET("/api/v2/outing/")
@@ -10,4 +11,9 @@ interface OutingAPI {
 
     @GET("/api/v2/outing/count")
     suspend fun getOutingCount(): CountResponse
+
+    @GET("/api/v2/outing/search")
+    suspend fun outingSearch(
+        @Query("name") name: String
+    ): List<OutingResponse>
 }
