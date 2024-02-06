44b7094f7eac3ed554de61592520af3ca0397f4d
GSM-Minseok <ms0818000@gmail.com>
2024-02-07 05:29:09 +0900

    :memo: :: Add QrcodeScanPreview.kt

---
+++ feature/qrcode-scan/src/main/java/com/goms/qrcode_scan/component/QrcodeScanPreview.kt
@@ -0,0 +1,55 @@
+package com.goms.qrcode_scan.component
+
+import android.content.Context
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.LifecycleOwner
+import com.goms.design_system.theme.GomsTheme
+
+@Composable
+fun QrcodeScanPreview(
+    context: Context,
+) {
+
+    val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+    val cameraController: LifecycleCameraController = remember { LifecycleCameraController(context) }
+
+    GomsTheme { colors, typography ->
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+        ) { innerPadding: PaddingValues ->
+            AndroidView(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                factory = { context ->
+                    PreviewView(context).apply {
+                        setBackgroundColor(Color.LightGray.toArgb())
+                        layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+                        scaleType = PreviewView.ScaleType.FILL_CENTER
+                        implementationMode = PreviewView.ImplementationMode.PERFORMANCE
+                    }.also { previewView ->
+                        previewView.controller = cameraController
+                        cameraController.bindToLifecycle(lifecycleOwner)
+                    }
+                },
+                onRelease = {
+                    cameraController.unbind()
+                }
+            )
+        }
+    }
+}
