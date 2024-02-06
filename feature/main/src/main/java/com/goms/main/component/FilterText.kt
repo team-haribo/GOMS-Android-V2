1b3e49b96e230da7e1eb03ff44b9a8666f1dc9ad
송현서 <s22026@gsm.hs.kr>
2024-02-06 21:08:46 +0900

    :lipstick: :: Add FilterText

---
+++ feature/main/src/main/java/com/goms/main/component/FilterText.kt
@@ -0,0 +1,31 @@
+package com.goms.main.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.goms.design_system.component.modifier.gomsClickable
+import com.goms.design_system.theme.GomsTheme
+
+@Composable
+fun FilterText(onFilterTextClick: () -> Unit) {
+    GomsTheme { colors, typography ->
+        Box(
+            modifier = Modifier
+                .height(40.dp)
+                .gomsClickable { onFilterTextClick() }
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = "필터",
+                style = typography.buttonLarge,
+                fontWeight = FontWeight.Normal,
+                color = colors.I5
+            )
+        }
+    }
+}
