3083832975ddfa5bfc57d2e58c2fca4e4f8196cb
GSM-Minseok <ms0818000@gmail.com>
2024-02-07 17:25:18 +0900

    :memo: :: Add QrcodeScanGuide.kt

--- feature/qrcode-scan/src/main/java/com/goms/qrcode_scan/component/QrcodeScanGuide.kt
+++ feature/qrcode-scan/src/main/java/com/goms/qrcode_scan/component/QrcodeScanGuide.kt
@@ -4,11 +4,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.goms.design_system.theme.GomsTheme
+<<<<<<< HEAD
 import com.goms.design_system.icon.QrScanGuideIcon
 @Composable
 fun QrcodeScanGuide() {
     GomsTheme { _, _ ->
         QrScanGuideIcon()
+=======
+import com.goms.model.enum.Authority
+import com.goms.ui.GomsFloatingButton
+
+@Composable
+fun QrcodeScanGuide() {
+    GomsTheme { _, _ ->
+        QrcodeScanGuide()
+>>>>>>> 9d1ba91 (:memo: :: Add QrcodeScanGuide.kt)
     }
 }
 
