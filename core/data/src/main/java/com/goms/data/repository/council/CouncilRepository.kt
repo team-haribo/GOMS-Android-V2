6fc9474b9677fb0fec3436881f14c49a45d9d4a8
송현서 <s22026@gsm.hs.kr>
2024-02-08 12:46:42 +0900

    :sparkles: :: Add get late list to CouncilRepository

--- core/data/src/main/java/com/goms/data/repository/council/CouncilRepository.kt
+++ core/data/src/main/java/com/goms/data/repository/council/CouncilRepository.kt
@@ -1,8 +1,12 @@
 package com.goms.data.repository.council
 
+import com.goms.model.response.council.LateResponse
 import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.LocalDate
 import java.util.UUID
 
 interface CouncilRepository {
     suspend fun deleteOuting(accountIdx: UUID): Flow<Unit>
+
+    suspend fun getLateList(date: LocalDate): Flow<List<LateResponse>>
 }
