67f23f03cfa4a1c84fbb6834a5e3758083fd0ad0
송현서 <s22026@gsm.hs.kr>
2024-02-11 17:50:52 +0900

    :sparkles: :: Add get student list to CouncilAPI

--- core/network/src/main/java/com/goms/network/api/CouncilAPI.kt
+++ core/network/src/main/java/com/goms/network/api/CouncilAPI.kt
@@ -1,6 +1,7 @@
 package com.goms.network.api
 
 import com.goms.model.response.council.LateResponse
+import com.goms.model.response.council.StudentResponse
 import kotlinx.datetime.LocalDate
 import retrofit2.http.DELETE
 import retrofit2.http.GET
@@ -9,6 +10,9 @@ import retrofit2.http.Query
 import java.util.UUID
 
 interface CouncilAPI {
+    @GET("/api/v2/student-council/accounts")
+    suspend fun getStudentList(): List<StudentResponse>
+
     @DELETE("/api/v2/student-council/outing/{accountIdx}")
     suspend fun deleteOuting(
         @Path("accountIdx") accountIdx: UUID
