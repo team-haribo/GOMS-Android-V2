5241a30ca25c3d87969ffcc31c167723f15951e3
송현서 <s22026@gsm.hs.kr>
2024-02-11 20:16:52 +0900

    :sparkles: :: Add change authority to CouncilAPI

--- core/network/src/main/java/com/goms/network/api/CouncilAPI.kt
+++ core/network/src/main/java/com/goms/network/api/CouncilAPI.kt
@@ -1,10 +1,13 @@
 package com.goms.network.api
 
+import com.goms.model.request.council.AuthorityRequest
 import com.goms.model.response.council.LateResponse
 import com.goms.model.response.council.StudentResponse
 import kotlinx.datetime.LocalDate
+import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.Path
 import retrofit2.http.Query
 import java.util.UUID
@@ -12,6 +15,11 @@ import java.util.UUID
 interface CouncilAPI {
     @GET("/api/v2/student-council/accounts")
     suspend fun getStudentList(): List<StudentResponse>
+
+    @PATCH("/api/v2/student-council/authority")
+    suspend fun changeAuthority(
+        @Body body: AuthorityRequest
+    )
 
     @DELETE("/api/v2/student-council/outing/{accountIdx}")
     suspend fun deleteOuting(
