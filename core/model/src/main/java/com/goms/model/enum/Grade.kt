6cd90f3e55666f1b314d5a5a7b22840753fe86a9
송현서 <s22026@gsm.hs.kr>
2024-02-12 16:51:41 +0900

    :sparkles: :: Modify Grade enum

--- core/model/src/main/java/com/goms/model/enum/Grade.kt
+++ core/model/src/main/java/com/goms/model/enum/Grade.kt
@@ -1,7 +1,7 @@
 package com.goms.model.enum
 
 enum class Grade(val value: String, val enum: Int) {
-    FIRST_GRADE("1학년", 1),
-    SECOND_GRADE("2학년", 2),
-    THIRD_GRADE("3학년", 3)
+    FIRST_GRADE("1학년", 8),
+    SECOND_GRADE("2학년", 7),
+    THIRD_GRADE("3학년", 6)
 }
