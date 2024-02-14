d0b858def31f9983eae9900c0cca69484e411c71
GSM-Minseok <ms0818000@gmail.com>
2024-02-15 02:57:45 +0900

    :memo: :: Add get permission camera

--- feature/qrcode-scan/build.gradle.kts
+++ feature/qrcode-scan/build.gradle.kts
@@ -15,11 +15,4 @@ dependencies {
     add("implementation", libs.findLibrary("camera.camera2").get())
     add("implementation", libs.findLibrary("camera.lifecycle").get())
     add("implementation", libs.findLibrary("camera.extensions").get())
-<<<<<<< HEAD
-
-    add("implementation", libs.findLibrary("mlkit").get())
-
-    add("implementation", libs.findLibrary("accompanist.permission").get())
-=======
->>>>>>> 9276a7d (:memo: :: Add qrcode module and set dependency)
 }
