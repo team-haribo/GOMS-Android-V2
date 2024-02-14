0e29434d9acbb0a5f3bdfe97fc531cc433f793ad
송현서 <s22026@gsm.hs.kr>
2024-02-14 18:53:47 +0900

    :sparkles: :: Add empty ui state

--- feature/main/src/main/java/com/goms/main/viewmodel/GetLateRankListUiState.kt
+++ feature/main/src/main/java/com/goms/main/viewmodel/GetLateRankListUiState.kt
@@ -4,6 +4,7 @@ import com.goms.model.response.late.RankResponse
 
 sealed interface GetLateRankListUiState {
     object Loading : GetLateRankListUiState
+    object Empty : GetLateRankListUiState
     data class Success(val getLateRankListResponse: List<RankResponse>) : GetLateRankListUiState
     data class Error(val exception: Throwable) : GetLateRankListUiState
 }
