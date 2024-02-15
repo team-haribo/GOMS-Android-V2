27d67f3b35c69fd37f3bb52891bfcd0768dc2cd8
GSM-Minseok <ms0818000@gmail.com>
2024-02-15 21:15:42 +0900

    :memo: :: Add OutingAPI outing function

--- core/network/src/main/java/com/goms/network/api/OutingAPI.kt
+++ core/network/src/main/java/com/goms/network/api/OutingAPI.kt
@@ -2,19 +2,10 @@ package com.goms.network.api
 
 import com.goms.model.response.outing.CountResponse
 import com.goms.model.response.outing.OutingResponse
-import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
 import retrofit2.http.Query
-import java.util.UUID
 
 interface OutingAPI {
-    @POST("/api/v2/outing/{outingUUID}")
-    suspend fun outing(
-        @Path("outingUUID") outingUUID: UUID
-    ): Response<Unit>
-
     @GET("/api/v2/outing/")
     suspend fun getOutingList(): List<OutingResponse>
 
