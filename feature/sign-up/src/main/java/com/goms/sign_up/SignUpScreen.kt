c29120d8cbbea36e58d762e864a0824433ea58d8
송현서 <s22026@gsm.hs.kr>
2024-02-16 17:58:18 +0900

    :sparkles: :: Processing loading status from SignUp

--- feature/sign-up/src/main/java/com/goms/sign_up/SignUpScreen.kt
+++ feature/sign-up/src/main/java/com/goms/sign_up/SignUpScreen.kt
@@ -13,10 +13,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -32,6 +34,7 @@ import com.goms.design_system.component.bottomsheet.SelectorBottomSheet
 import com.goms.design_system.component.button.ButtonState
 import com.goms.design_system.component.button.GomsBackButton
 import com.goms.design_system.component.button.GomsButton
+import com.goms.design_system.component.indicator.GomsCircularProgressIndicator
 import com.goms.design_system.component.textfield.GomsBottomSheetTextField
 import com.goms.design_system.component.textfield.GomsTextField
 import com.goms.design_system.theme.GomsTheme
@@ -56,15 +59,6 @@ fun SignUpRoute(
         val gender by viewModel.gender.collectAsStateWithLifecycle()
         val major by viewModel.major.collectAsStateWithLifecycle()
 
-        when (sendNumberUiState) {
-            is Result.Loading -> Unit
-            is Result.Success -> {
-                onNumberClick()
-                viewModel.initSendNumber()
-            }
-            is Result.Error -> {}
-        }
-
         SignUpScreen(
             name = name,
             email = email,
@@ -74,8 +68,11 @@ fun SignUpRoute(
             onEmailChange = viewModel::onEmailChange,
             onGenderChange = viewModel::onGenderChange,
             onMajorChange =  viewModel::onMajorChange,
+            sendNumberUiState = sendNumberUiState,
             onBackClick = onBackClick,
-            signUpCallBack = { viewModel.sendNumber(body = SendNumberRequest("${viewModel.email.value}@gsm.hs.kr")) }
+            onNumberClick = onNumberClick,
+            signUpCallBack = { viewModel.sendNumber(body = SendNumberRequest("${viewModel.email.value}@gsm.hs.kr")) },
+            initCallBack = { viewModel.initSendNumber() }
         )
     }
 }
@@ -91,14 +88,18 @@ fun SignUpScreen(
     onEmailChange: (String) -> Unit,
     onGenderChange: (String) -> Unit,
     onMajorChange: (String) -> Unit,
+    sendNumberUiState: Result<Unit>,
     onBackClick: () -> Unit,
-    signUpCallBack: () -> Unit
+    onNumberClick: () -> Unit,
+    signUpCallBack: () -> Unit,
+    initCallBack: () -> Unit
 ) {
     val focusManager = LocalFocusManager.current
     val isKeyboardOpen by keyboardAsState()
 
     var onGenderBottomSheetOpenClick by rememberSaveable { mutableStateOf(false) }
     var onMajorBottomSheetOpenClick by rememberSaveable { mutableStateOf(false) }
+    var isLoading by remember { mutableStateOf(false) }
 
     LaunchedEffect(isKeyboardOpen) {
         if (!isKeyboardOpen) {
@@ -106,105 +107,119 @@ fun SignUpScreen(
         }
     }
 
+    DisposableEffect(sendNumberUiState) {
+        when (sendNumberUiState) {
+            is Result.Loading -> Unit
+            is Result.Success -> onNumberClick()
+            is Result.Error -> isLoading = false
+        }
+        onDispose { initCallBack() }
+    }
+
     lockScreenOrientation(orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
     GomsTheme { colors, typography ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(colors.BLACK)
-                .statusBarsPadding()
-                .navigationBarsPadding()
-                .pointerInput(Unit) {
-                    detectTapGestures {
+        if (isLoading) {
+            GomsCircularProgressIndicator()
+        } else {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colors.BLACK)
+                    .statusBarsPadding()
+                    .navigationBarsPadding()
+                    .pointerInput(Unit) {
+                        detectTapGestures {
+                            focusManager.clearFocus()
+                        }
+                    }
+            ) {
+                GomsBackButton {
+                    onBackClick()
+                }
+                Column(
+                    modifier = Modifier.padding(horizontal = 20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    SignUpText(modifier = Modifier.align(Alignment.Start))
+                    Spacer(modifier = Modifier.weight(1.1f))
+                    GomsTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        placeHolder = "이름",
+                        setText = name,
+                        onValueChange = onNameChange,
+                        isEmail = false,
+                        singleLine = true
+                    )
+                    GomsTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                        placeHolder = "이메일",
+                        setText = email,
+                        onValueChange = onEmailChange,
+                        singleLine = true
+                    )
+                    GomsBottomSheetTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        placeHolder = "성별",
+                        setText = gender,
+                        onValueChange = onGenderChange,
+                        readOnly = true,
+                        singleLine = true
+                    ) {
+                        onGenderBottomSheetOpenClick = true
                         focusManager.clearFocus()
                     }
+                    GomsBottomSheetTextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                        placeHolder = "과",
+                        setText = major,
+                        onValueChange = onMajorChange,
+                        readOnly = true,
+                        singleLine = true
+                    ) {
+                        onMajorBottomSheetOpenClick = true
+                        focusManager.clearFocus()
+                    }
+                    Spacer(modifier = Modifier.weight(1f))
+                    GomsButton(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = "인증번호 받기",
+                        state = if (name.isNotBlank() && email.isNotBlank() && gender.isNotBlank() && major.isNotBlank()) ButtonState.Normal
+                        else ButtonState.Enable
+                    ) {
+                        signUpCallBack()
+                        isLoading = true
+                    }
+                    Spacer(modifier = Modifier.height(100.dp))
                 }
-        ) {
-            GomsBackButton {
-                onBackClick()
             }
-            Column(
-                modifier = Modifier.padding(horizontal = 20.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                SignUpText(modifier = Modifier.align(Alignment.Start))
-                Spacer(modifier = Modifier.weight(1.1f))
-                GomsTextField(
+            if (onGenderBottomSheetOpenClick) {
+                SelectorBottomSheet(
                     modifier = Modifier.fillMaxWidth(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                    placeHolder = "이름",
-                    setText = name,
-                    onValueChange = onNameChange,
-                    isEmail = false,
-                    singleLine = true
+                    title = "성별",
+                    list = listOf(Gender.MAN.value, Gender.WOMAN.value),
+                    selected = gender,
+                    itemChange = onGenderChange,
+                    closeSheet = {
+                        onGenderBottomSheetOpenClick = false
+                    }
                 )
-                GomsTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                    placeHolder = "이메일",
-                    setText = email,
-                    onValueChange = onEmailChange,
-                    singleLine = true
-                )
-                GomsBottomSheetTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                    placeHolder = "성별",
-                    setText = gender,
-                    onValueChange = onGenderChange,
-                    readOnly = true,
-                    singleLine = true
-                ) {
-                    onGenderBottomSheetOpenClick = true
-                    focusManager.clearFocus()
-                }
-                GomsBottomSheetTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-                    placeHolder = "과",
-                    setText = major,
-                    onValueChange = onMajorChange,
-                    readOnly = true,
-                    singleLine = true
-                ) {
-                    onMajorBottomSheetOpenClick = true
-                    focusManager.clearFocus()
-                }
-                Spacer(modifier = Modifier.weight(1f))
-                GomsButton(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = "인증번호 받기",
-                    state = if (name.isNotBlank() && email.isNotBlank() && gender.isNotBlank() && major.isNotBlank()) ButtonState.Normal
-                    else ButtonState.Enable
-                ) {
-                    signUpCallBack()
-                }
-                Spacer(modifier = Modifier.height(100.dp))
             }
-        }
-        if (onGenderBottomSheetOpenClick) {
-            SelectorBottomSheet(
-                modifier = Modifier.fillMaxWidth(),
-                title = "성별",
-                list = listOf(Gender.MAN.value, Gender.WOMAN.value),
-                selected = gender,
-                itemChange = onGenderChange,
-                closeSheet = {
-                    onGenderBottomSheetOpenClick = false
-                }
-            )
-        }
-        if (onMajorBottomSheetOpenClick) {
-            SelectorBottomSheet(
-                modifier = Modifier.fillMaxWidth(),
-                title = "과",
-                list = listOf(Major.SW_DEVELOP.value, Major.SMART_IOT.value, Major.AI.value),
-                selected = major,
-                itemChange = onMajorChange,
-                closeSheet = {
-                    onMajorBottomSheetOpenClick = false
-                }
-            )
+            if (onMajorBottomSheetOpenClick) {
+                SelectorBottomSheet(
+                    modifier = Modifier.fillMaxWidth(),
+                    title = "과",
+                    list = listOf(Major.SW_DEVELOP.value, Major.SMART_IOT.value, Major.AI.value),
+                    selected = major,
+                    itemChange = onMajorChange,
+                    closeSheet = {
+                        onMajorBottomSheetOpenClick = false
+                    }
+                )
+            }
         }
     }
 }
