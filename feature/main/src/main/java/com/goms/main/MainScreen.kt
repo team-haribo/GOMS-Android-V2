4a4f2fe51ec25047cb3e6d6b81655f4995e39190
송현서 <s22026@gsm.hs.kr>
2024-02-23 23:19:17 +0900

    :fire: :: Remove log import

--- feature/main/src/main/java/com/goms/main/MainScreen.kt
+++ feature/main/src/main/java/com/goms/main/MainScreen.kt
@@ -1,6 +1,5 @@
 package com.goms.main
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
