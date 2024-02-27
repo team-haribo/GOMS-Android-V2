8dabcfef7eb4b1754b3841b44c6bcd9af1940d12
GSM-Minseok <ms0818000@gmail.com>
2024-02-27 20:34:05 +0900

    :memo: :: Add generateAdapter

--- core/model/src/main/java/com/goms/model/response/council/OutingUUIDResponse.kt
+++ core/model/src/main/java/com/goms/model/response/council/OutingUUIDResponse.kt
@@ -1,8 +1,9 @@
 package com.goms.model.response.council
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import java.util.UUID
-
+@JsonClass(generateAdapter = true)
 data class OutingUUIDResponse(
     @Json(name = "outingUUID") val outingUUID: String,
 )
