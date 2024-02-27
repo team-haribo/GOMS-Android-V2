5604fa4530b4972eb1cef1dcd77dc81783f985a3
GSM-Minseok <ms0818000@gmail.com>
2024-02-27 17:33:39 +0900

    :memo: :: Add OutingUUIDResponse

---
+++ core/model/src/main/java/com/goms/model/response/council/OutingUUIDResponse.kt
@@ -0,0 +1,8 @@
+package com.goms.model.response.council
+
+import com.squareup.moshi.Json
+import java.util.UUID
+
+data class OutingUUIDResponse(
+    @Json(name = "outingUUID") val outingUUID: UUID,
+)
