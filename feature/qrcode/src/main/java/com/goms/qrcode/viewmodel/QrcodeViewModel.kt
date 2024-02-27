770dd3cc12355a4534c132ef14bea9d334cb407e
GSM-Minseok <ms0818000@gmail.com>
2024-02-27 17:33:17 +0900

    :memo: :: Use GetOutingUUIDUiState

--- feature/qrcode/src/main/java/com/goms/qrcode/viewmodel/QrcodeViewModel.kt
+++ feature/qrcode/src/main/java/com/goms/qrcode/viewmodel/QrcodeViewModel.kt
@@ -8,8 +8,10 @@ import com.goms.common.result.Result
 import com.goms.common.result.asResult
 import com.goms.domain.auth.LoginUseCase
 import com.goms.domain.auth.SaveTokenUseCase
+import com.goms.domain.council.GetOutingUUIDUseCase
 import com.goms.domain.outing.OutingUseCase
 import com.goms.model.request.auth.SignUpRequest
+import com.goms.model.response.council.OutingUUIDResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -21,10 +23,14 @@ import javax.inject.Inject
 
 @HiltViewModel
 class QrcodeViewModel @Inject constructor(
-    private val outingUseCase: OutingUseCase
+    private val outingUseCase: OutingUseCase,
+    private val getOutingUUIDUseCase: GetOutingUUIDUseCase
 ) : ViewModel() {
     private val _outingState = MutableStateFlow<Result<Unit>>(Result.Loading)
     val outingState = _outingState.asStateFlow()
+
+    private val _getOutingUUIDState = MutableStateFlow<GetOutingUUIDUiState>(GetOutingUUIDUiState.Loading)
+    val getOutingUUIDState = _getOutingUUIDState.asStateFlow()
 
     fun outing(outingUUID: UUID) = viewModelScope.launch {
         outingUseCase(outingUUID = outingUUID)
@@ -36,6 +42,17 @@ class QrcodeViewModel @Inject constructor(
                 }
             }.onFailure {
                 _outingState.value = Result.Error(it)
+            }
+    }
+    fun getOutingUUID() = viewModelScope.launch {
+        getOutingUUIDUseCase()
+            .asResult()
+            .collectLatest { result ->
+                when (result) {
+                    is Result.Loading -> _getOutingUUIDState.value = GetOutingUUIDUiState.Loading
+                    is Result.Success -> _getOutingUUIDState.value = GetOutingUUIDUiState.Success(result.data)
+                    is Result.Error -> _getOutingUUIDState.value = GetOutingUUIDUiState.Error(result.exception)
+                }
             }
     }
 }
