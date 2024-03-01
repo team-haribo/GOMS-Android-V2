343b2f12a1eabbf4ffd9f99630ac314df5855bed
송현서 <s22026@gsm.hs.kr>
2024-03-01 13:54:54 +0900

    :sparkles: :: Change password regular expression

--- core/design-system/src/main/java/com/goms/design_system/util/isStrongPassword.kt
+++ core/design-system/src/main/java/com/goms/design_system/util/isStrongPassword.kt
@@ -1,6 +1,6 @@
 package com.goms.design_system.util
 
 fun isStrongPassword(password: String): Boolean {
-    val regex = Regex("^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#\$%^&*(),.?\":{}|<>])(.{12,})\$")
+    val regex = Regex("^(?=.[a-zA-Z])(?=.[!@#\$%^&?~])[a-zA-Z!@#\$%^&?~]{6,15}\$")
     return regex.matches(password)
 }
