83ac223783f2ef042c28efdcf8da626e34a2a5c2
송현서 <s22026@gsm.hs.kr>
2024-03-04 10:35:09 +0900

    :truck: :: Move isStrongPassword

--- core/design-system/src/main/java/com/goms/design_system/util/isStrongPassword.kt
+++
@@ -1,6 +0,0 @@
-package com.goms.design_system.util
-
-fun isStrongPassword(password: String): Boolean {
-    val regex = Regex("^(?=.[a-zA-Z])(?=.[!@#\$%^&?~])[a-zA-Z!@#\$%^&?~]{6,15}\$")
-    return regex.matches(password)
-}
