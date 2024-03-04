04c97e32f417a6d2d3cd18f2af0c96ce15e23486
수잉 <djswpsrkaksskfskf@gmail.com>
2024-03-04 16:17:49 +0900

    :sparkles: :: Re-Password Text

---
+++ feature/re-password/src/main/java/com/goms/re_password/component/RePasswordText.kt
@@ -0,0 +1,21 @@
+package com.goms.re_password.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import com.goms.design_system.theme.GomsTheme
+
+
+@Composable
+fun RePasswordText(modifier: Modifier){
+    GomsTheme { colors, typography ->
+        Text(
+            modifier = modifier,
+            text = "회원가입",
+            style = typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            color = colors.WHITE
+        )
+    }
+}
