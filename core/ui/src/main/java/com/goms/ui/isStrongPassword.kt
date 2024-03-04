6d4a27a419c0ce0f58c45a29c657596a2446506b
송현서 <s22026@gsm.hs.kr>
2024-03-04 13:58:59 +0900

    :sparkles: :: Modify isStrongPassword

--- core/ui/src/main/java/com/goms/ui/isStrongPassword.kt
+++ core/ui/src/main/java/com/goms/ui/isStrongPassword.kt
@@ -1,6 +1,6 @@
 package com.goms.ui
 
 fun isStrongPassword(password: String): Boolean {
-    val regex = Regex("^(?=.[a-zA-Z])(?=.[0-9])(?=.[!@#\$%^&?~])[a-zA-Z0-9!@#\$%^&*?~]{6,15}\$")
+    val regex = Regex("^(?=.*[a-zA-Z])(?=.*[!@#\$%^+=-?<>])(?=.*[0-9]).{6,15}\$")
     return regex.matches(password)
 }
