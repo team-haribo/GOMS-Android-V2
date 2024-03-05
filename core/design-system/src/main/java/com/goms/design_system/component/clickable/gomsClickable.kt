70cc3ee3ebea84b43ca75acdc3b9055c88ba52ee
송현서 <s22026@gsm.hs.kr>
2024-03-05 13:53:56 +0900

    :sparkles: :: Add ripple event

--- core/design-system/src/main/java/com/goms/design_system/component/clickable/gomsClickable.kt
+++ core/design-system/src/main/java/com/goms/design_system/component/clickable/gomsClickable.kt
@@ -3,15 +3,18 @@ package com.goms.design_system.component.clickable
 import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.debugInspectorInfo
 import androidx.compose.ui.semantics.Role
 
 fun Modifier.gomsClickable(
     enabled: Boolean = true,
     isIndication: Boolean = false,
+    rippleColor: Color? = null,
     onClickLabel: String? = null,
     role: Role? = null,
     onClick: () -> Unit
@@ -30,7 +33,13 @@ fun Modifier.gomsClickable(
         onClickLabel = onClickLabel,
         onClick = { multipleEventsCutter.processEvent { onClick() } },
         role = role,
-        indication = if (isIndication) LocalIndication.current else null,
+        indication = if (isIndication) {
+            rippleColor?.let {
+                rememberRipple(
+                    color = it
+                )
+            } ?: LocalIndication.current
+        } else null,
         interactionSource = remember { MutableInteractionSource() }
     )
 }
