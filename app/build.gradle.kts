31fd267085d874e534fae392f9baa95a8af6fc70
송현서 <s22026@gsm.hs.kr>
2024-03-06 18:01:14 +0900

    :heavy_plus_sign: :: Add core ui module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation(project(":core:data"))
     implementation(project(":core:model"))
     implementation(project(":core:domain"))
+    implementation(project(":core:ui"))
     implementation(project(":feature:login"))
     implementation(project(":feature:sign-up"))
     implementation(project(":feature:main"))
