c2e4590ac275d7d20d8d6a56ad2132dda821dd8a
GSM-Minseok <ms0818000@gmail.com>
2024-03-08 00:49:29 +0900

    :sparkles: :: Add GetProfileUiState.kt

---
+++ feature/setting/src/main/java/com/goms/setting/viewmodel/GetProfileUiState.kt
@@ -0,0 +1,9 @@
+package com.goms.setting.viewmodel
+
+import com.goms.model.response.account.ProfileResponse
+
+sealed interface GetProfileUiState {
+    object Loading : GetProfileUiState
+    data class Success(val getProfileResponse: ProfileResponse) : GetProfileUiState
+    data class Error(val exception: Throwable) : GetProfileUiState
+}
