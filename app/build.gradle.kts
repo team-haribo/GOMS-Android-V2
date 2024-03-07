a4e6e625591f415253d76f3199c044ff14b82120
GSM-Minseok <ms0818000@gmail.com>
2024-03-08 00:41:14 +0900

    :heavy_plus_sign: :: Add androidx wear compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation(project(":feature:login"))
     implementation(project(":feature:sign-up"))
     implementation(project(":feature:main"))
+    implementation(project(":feature:setting"))
     implementation(project(":feature:qrcode"))
 
     implementation(libs.junit)
