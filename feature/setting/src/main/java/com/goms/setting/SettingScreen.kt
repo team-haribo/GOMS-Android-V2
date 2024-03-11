fbf2b9223bd2dbb6dc96788dca8f69add920b980
GSM-Minseok <ms0818000@gmail.com>
2024-03-11 20:16:00 +0900

    :sparkles: :: Add admin page

--- feature/setting/src/main/java/com/goms/setting/SettingScreen.kt
+++ feature/setting/src/main/java/com/goms/setting/SettingScreen.kt
@@ -13,9 +13,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -23,6 +25,7 @@ import com.goms.design_system.component.button.ButtonState
 import com.goms.design_system.component.button.GomsBackButton
 import com.goms.design_system.component.button.GomsButton
 import com.goms.design_system.theme.GomsTheme
+import com.goms.model.enum.Authority
 import com.goms.setting.component.SettingProfileCard
 import com.goms.setting.component.PasswordChangeButton
 import com.goms.setting.component.SelectThemeDropDown
@@ -37,6 +40,7 @@ fun SettingRoute(
 ) {
     SettingViewModelProvider(viewModelStoreOwner = viewModelStoreOwner) { viewModel ->
         val getProfileUiState by viewModel.getProfileUiState.collectAsStateWithLifecycle()
+        val role by viewModel.role.collectAsState(initial = "")
 
         SettingScreen(
             onBackClick = onBackClick,
@@ -44,6 +48,7 @@ fun SettingRoute(
                 viewModel.initGetProfile()
                 viewModel.getProfile()
             },
+            role = role,
             getProfileUiState = getProfileUiState
         )
     }
@@ -52,6 +57,7 @@ fun SettingRoute(
 fun SettingScreen(
     onBackClick: () -> Unit,
     getProfile: () -> Unit,
+    role: String,
     getProfileUiState: GetProfileUiState
 ) {
     LaunchedEffect("mypage init") { getProfile() }
@@ -74,25 +80,37 @@ fun SettingScreen(
             Spacer(modifier = Modifier.height(24.dp))
             SelectThemeDropDown(modifier = Modifier.padding(horizontal = 20.dp))
             Spacer(modifier = Modifier.height(24.dp))
-            SettingSwitchComponent(
-                modifier = Modifier.padding(horizontal = 28.dp),
-                title = "외출제 푸시 알림",
-                detail = "외출할 시간이 될 때마다 알려드려요",
-                switchOnBackground = colors.P5,
-                switchOffBackground = colors.G4,
-                onFunctionOff = { Log.d("testt","off1") },
-                onFunctionOn = { Log.d("testt","on1") }
-            )
-            Spacer(modifier = Modifier.height(32.dp))
-            SettingSwitchComponent(
-                modifier = Modifier.padding(horizontal = 28.dp),
-                title = "카메라 바로 켜기",
-                detail = "앱을 실행하면 즉시 카메라가 켜져요",
-                switchOnBackground = colors.P5,
-                switchOffBackground = colors.G4,
-                onFunctionOff = { Log.d("testt","off2") },
-                onFunctionOn = { Log.d("testt","on2") }
-            )
+            if(role == Authority.ROLE_STUDENT.name) {
+                SettingSwitchComponent(
+                    modifier = Modifier.padding(horizontal = 28.dp),
+                    title = "외출제 푸시 알림",
+                    detail = "외출할 시간이 될 때마다 알려드려요",
+                    switchOnBackground = colors.P5,
+                    switchOffBackground = colors.G4,
+                    onFunctionOff = { Log.d("testt","off1") },
+                    onFunctionOn = { Log.d("testt","on1") }
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                SettingSwitchComponent(
+                    modifier = Modifier.padding(horizontal = 28.dp),
+                    title = "카메라 바로 켜기",
+                    detail = "앱을 실행하면 즉시 카메라가 켜져요",
+                    switchOnBackground = colors.P5,
+                    switchOffBackground = colors.G4,
+                    onFunctionOff = {  },
+                    onFunctionOn = { }
+                )
+            } else {
+                SettingSwitchComponent(
+                    modifier = Modifier.padding(horizontal = 28.dp),
+                    title = "Qr 생성 바로 켜기",
+                    detail = "앱을 실행하면 즉시 Qr코드를 생성해요",
+                    switchOnBackground = colors.A7,
+                    switchOffBackground = colors.G4,
+                    onFunctionOff = { },
+                    onFunctionOn = { }
+                )
+            }
         }
         Box(
             modifier = Modifier
