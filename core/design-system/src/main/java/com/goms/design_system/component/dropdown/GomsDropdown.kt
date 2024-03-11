289d1c419bf01b5cc0ad5d9e533bd0ebf47d5236
GSM-Minseok <ms0818000@gmail.com>
2024-03-11 20:00:34 +0900

    :memo: :: Set directory

--- core/design-system/src/main/java/com/goms/design_system/component/dropdown/GomsDropdown.kt
+++ core/design-system/src/main/java/com/goms/design_system/component/dropdown/GomsDropdown.kt
@@ -1,6 +1,5 @@
-package com.goms.design_system.component
+package com.goms.design_system.component.dropdown
 
-import android.text.style.BackgroundColorSpan
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -10,18 +9,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
