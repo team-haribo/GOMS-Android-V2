fa64339988bc537326c374b45ef0ac68ad37bec3
GSM-Minseok <ms0818000@gmail.com>
2024-03-12 15:31:52 +0900

    :sparkles: :: Modify LogoutUseCase.kt

--- core/domain/src/main/java/com/goms/domain/auth/LogoutUseCase.kt
+++ core/domain/src/main/java/com/goms/domain/auth/LogoutUseCase.kt
@@ -3,7 +3,7 @@ package com.goms.domain.auth
 import com.goms.data.repository.auth.AuthRepository
 import javax.inject.Inject
 
-class LogoutUseCase@Inject constructor(
+class LogoutUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
     suspend operator fun invoke() = kotlin.runCatching {
