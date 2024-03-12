2a12c8ce5b218d20ed2d091f7853e81ae02d4c31
GSM-Minseok <ms0818000@gmail.com>
2024-03-12 15:06:45 +0900

    :sparkles: :: Use LogoutUseCase

--- feature/setting/src/main/java/com/goms/setting/viewmodel/SettingViewModel.kt
+++ feature/setting/src/main/java/com/goms/setting/viewmodel/SettingViewModel.kt
@@ -3,22 +3,30 @@ package com.goms.setting.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.goms.common.network.errorHandling
 import com.goms.common.result.Result
 import com.goms.common.result.asResult
 import com.goms.datastore.AuthTokenDataSource
 import com.goms.domain.account.GetProfileUseCase
+import com.goms.domain.auth.LogoutUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import java.util.UUID
 import javax.inject.Inject
 @HiltViewModel
 class SettingViewModel @Inject constructor (
     private val authTokenDataSource: AuthTokenDataSource,
     private val getProfileUseCase: GetProfileUseCase,
+    private val logoutUseCase: LogoutUseCase
 ) : ViewModel() {
     val role = authTokenDataSource.getAuthority()
+
+    private val _logoutState = MutableStateFlow<LogoutUiState>(LogoutUiState.Loading)
+    val logoutState = _logoutState.asStateFlow()
 
     private val _getProfileUiState = MutableStateFlow<GetProfileUiState>(GetProfileUiState.Loading)
     val getProfileUiState = _getProfileUiState.asStateFlow()
@@ -38,5 +46,25 @@ class SettingViewModel @Inject constructor (
 
     fun initGetProfile() {
         _getProfileUiState.value = GetProfileUiState.Loading
+    }
+
+    fun logout() = viewModelScope.launch {
+        logoutUseCase()
+            .onSuccess {
+                it.catch {  remoteError ->
+                    _logoutState.value = LogoutUiState.Error(remoteError)
+                    remoteError.errorHandling(
+                        badRequestAction = {  }
+                    )
+                }.collect {
+                    Log.d("testt","suc")
+                    _logoutState.value = LogoutUiState.Success
+                }
+            }.onFailure {
+                _logoutState.value = LogoutUiState.Error(it)
+                it.errorHandling(
+                    badRequestAction = { }
+                )
+            }
     }
 }
