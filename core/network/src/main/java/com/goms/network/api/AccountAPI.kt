ec5aa27c083aa114661bd5e0ffb9c94fade5c6c9
GSM-Minseok <ms0818000@gmail.com>
2024-03-13 08:17:42 +0900

    :memo: :: Add uploadProfileImage api

--- core/network/src/main/java/com/goms/network/api/AccountAPI.kt
+++ core/network/src/main/java/com/goms/network/api/AccountAPI.kt
@@ -1,9 +1,20 @@
 package com.goms.network.api
 
 import com.goms.model.response.account.ProfileResponse
+import okhttp3.MultipartBody
 import retrofit2.http.GET
+import retrofit2.http.Multipart
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Path
 
 interface AccountAPI {
     @GET("/api/v2/account/profile")
     suspend fun getProfile(): ProfileResponse
+    @Multipart
+    @POST("/api/v2/account/image")
+    suspend fun uploadProfileImage(
+        @Part file: MultipartBody.Part
+    )
 }
