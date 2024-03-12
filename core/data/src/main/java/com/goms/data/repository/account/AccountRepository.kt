83b99a17e9c45003116ccf8d151a2d899004283d
GSM-Minseok <ms0818000@gmail.com>
2024-03-13 08:18:10 +0900

    :memo: :: Add uploadProfileImage repository

--- core/data/src/main/java/com/goms/data/repository/account/AccountRepository.kt
+++ core/data/src/main/java/com/goms/data/repository/account/AccountRepository.kt
@@ -2,7 +2,10 @@ package com.goms.data.repository.account
 
 import com.goms.model.response.account.ProfileResponse
 import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
 
 interface AccountRepository {
     suspend fun getProfile(): Flow<ProfileResponse>
+
+    suspend fun uploadProfileImage(file: MultipartBody.Part): Flow<Unit>
 }
