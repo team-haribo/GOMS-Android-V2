4b3f4b603ab0d0403b52991dae4b036712ec999d
수잉 <djswpsrkaksskfskf@gmail.com>
2024-03-17 23:48:26 +0900

    :sparkles: :: Add account repassword DataSource

--- core/network/src/main/java/com/goms/network/datasource/account/AccountDataSource.kt
+++ core/network/src/main/java/com/goms/network/datasource/account/AccountDataSource.kt
@@ -1,12 +1,14 @@
 package com.goms.network.datasource.account
 
+import com.goms.model.request.account.RePasswordRequest
 import com.goms.model.response.account.ProfileResponse
 import kotlinx.coroutines.flow.Flow
 import okhttp3.MultipartBody
-import retrofit2.http.Multipart
 
 interface AccountDataSource {
     suspend fun getProfile(): Flow<ProfileResponse>
 
     suspend fun uploadProfileImage(file: MultipartBody.Part): Flow<Unit>
+
+    suspend fun rePassword(body: RePasswordRequest): Flow<Unit>
 }
