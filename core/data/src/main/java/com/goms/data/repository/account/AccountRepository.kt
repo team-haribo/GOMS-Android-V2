efaa6f495bd10bcc137e20ba972acb2ae223cd84
수잉 <djswpsrkaksskfskf@gmail.com>
2024-03-18 01:07:28 +0900

    :sparkles: :: Add account repassword Repository

--- core/data/src/main/java/com/goms/data/repository/account/AccountRepository.kt
+++ core/data/src/main/java/com/goms/data/repository/account/AccountRepository.kt
@@ -1,5 +1,6 @@
 package com.goms.data.repository.account
 
+import com.goms.model.request.account.RePasswordRequest
 import com.goms.model.response.account.ProfileResponse
 import kotlinx.coroutines.flow.Flow
 import okhttp3.MultipartBody
@@ -8,4 +9,6 @@ interface AccountRepository {
     suspend fun getProfile(): Flow<ProfileResponse>
 
     suspend fun uploadProfileImage(file: MultipartBody.Part): Flow<Unit>
+
+    suspend fun rePassword(body: RePasswordRequest): Flow<Unit>
 }
