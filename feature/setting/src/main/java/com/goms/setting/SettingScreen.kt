d0f0668b4ea7aa2d97905a381a6ed7e07c346556
송현서 <s22026@gsm.hs.kr>
2024-03-20 15:29:31 +0900

    :sparkles: :: Remove dat

--- feature/setting/src/main/java/com/goms/setting/SettingScreen.kt
+++ feature/setting/src/main/java/com/goms/setting/SettingScreen.kt
@@ -116,7 +116,7 @@ fun SettingScreen(
         }
 
         is LogoutUiState.Error -> {
-            onErrorToast(logoutUiState.exception, "로그아웃에 실패 했습니다.")
+            onErrorToast(logoutUiState.exception, "로그아웃에 실패 했습니다")
         }
     }
 
