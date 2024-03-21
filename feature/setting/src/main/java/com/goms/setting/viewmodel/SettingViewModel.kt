82b81657564fbe69037d516a1f1c8f3d4f29b9da
GSM-Minseok <ms0818000@gmail.com>
2024-03-21 21:57:09 +0900

    :sparkles: :: add Get theme value

--- feature/setting/src/main/java/com/goms/setting/viewmodel/SettingViewModel.kt
+++ feature/setting/src/main/java/com/goms/setting/viewmodel/SettingViewModel.kt
@@ -7,15 +7,18 @@ import androidx.lifecycle.viewModelScope
 import com.goms.common.result.Result
 import com.goms.common.result.asResult
 import com.goms.data.repository.auth.AuthRepository
+import com.goms.data.repository.setting.SettingRepository
 import com.goms.domain.account.GetProfileUseCase
 import com.goms.domain.account.UploadProfileImageUseCase
 import com.goms.domain.auth.LogoutUseCase
+import com.goms.domain.setting.SaveThemeUseCase
 import com.goms.setting.util.getMultipartFile
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -24,9 +27,16 @@ class SettingViewModel @Inject constructor (
     private val getProfileUseCase: GetProfileUseCase,
     private val uploadProfileImageUseCase: UploadProfileImageUseCase,
     private val logoutUseCase: LogoutUseCase,
+    private val settingRepository: SettingRepository,
+    private val saveThemeUseCase: SaveThemeUseCase,
     private val authRepository: AuthRepository
 ) : ViewModel() {
     val role = authRepository.getRole()
+
+    val settingInfo = MutableStateFlow<List<String>>(emptyList())
+
+    private val _setThemeState = MutableStateFlow<SetThemeUiState>(SetThemeUiState.Loading)
+    val setThemeState = _setThemeState.asStateFlow()
 
     private val _logoutState = MutableStateFlow<LogoutUiState>(LogoutUiState.Loading)
     val logoutState = _logoutState.asStateFlow()
@@ -71,6 +81,17 @@ class SettingViewModel @Inject constructor (
 
     fun initProfileImage() {
         _profileImageUiState.value = ProfileImageUiState.Loading
+    }
+
+    fun initSetTheme() {
+        _setThemeState.value = SetThemeUiState.Loading
+    }
+    suspend fun getThemeValue() {
+        val themeValue = settingRepository.getThemeValue().first().replace("\"","")
+        val alarmValue = settingRepository.getAlarmValue().first().replace("\"","")
+        val qrcodeValue = settingRepository.getQrcodeValue().first().replace("\"","")
+
+        settingInfo.emit(listOf(themeValue, alarmValue, qrcodeValue))
     }
 
 
