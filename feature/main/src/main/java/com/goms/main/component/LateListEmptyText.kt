e5e5c0400c98f956e2d4bb10d30c28aeb55b7462
송현서 <s22026@gsm.hs.kr>
2024-03-25 15:10:18 +0900

    :lipstick: :: Add center vertically

--- feature/main/src/main/java/com/goms/main/component/LateListEmptyText.kt
+++ feature/main/src/main/java/com/goms/main/component/LateListEmptyText.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
@@ -24,7 +25,8 @@ fun LateListEmptyText() {
             .clip(RoundedCornerShape(12.dp))
             .background(colors.G1)
             .padding(horizontal = 16.dp, vertical = 8.dp),
-        horizontalArrangement = Arrangement.spacedBy(4.dp)
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         FireIcon(tint = colors.WHITE)
         Text(
