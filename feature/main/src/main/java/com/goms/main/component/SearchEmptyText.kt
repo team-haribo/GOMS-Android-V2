285ea4d2f2a8ac6e91650b5feb2c576654116a9e
송현서 <s22026@gsm.hs.kr>
2024-03-25 14:14:13 +0900

    :lipstick: :: Modify SearchEmptyText

--- feature/main/src/main/java/com/goms/main/component/SearchEmptyText.kt
+++ feature/main/src/main/java/com/goms/main/component/SearchEmptyText.kt
@@ -1,39 +1,37 @@
 package com.goms.main.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.goms.design_system.icon.CoffeeIcon
 import com.goms.design_system.theme.GomsTheme.colors
 import com.goms.design_system.theme.GomsTheme.typography
 
 @Composable
 fun SearchEmptyText() {
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(12.dp))
+            .background(colors.G1)
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
     ) {
+        CoffeeIcon(tint = colors.WHITE)
         Text(
-            text = "\uD83E\uDD14",
-            fontSize = 80.sp,
-            fontWeight = FontWeight.Bold,
+            text = "검색 결과가 없습니다...",
+            style = typography.textSmall,
+            fontWeight = FontWeight.SemiBold,
             color = colors.WHITE
-        )
-        Text(
-            text = "검색 결과가 없습니다\n" +
-                    "검색 내용이 잘못되진 않았나요?",
-            style = typography.textMedium,
-            fontWeight = FontWeight.Normal,
-            color = colors.G4,
-            textAlign = TextAlign.Center
         )
     }
 }
