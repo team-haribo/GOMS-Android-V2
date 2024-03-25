dedafeebb151b3cec132b5910d72fe84dfe2a8f0
송현서 <s22026@gsm.hs.kr>
2024-03-25 15:13:47 +0900

    :lipstick: :: Remove shimmer box

--- feature/main/src/main/java/com/goms/main/component/MainOutingCard.kt
+++ feature/main/src/main/java/com/goms/main/component/MainOutingCard.kt
@@ -1,6 +1,5 @@
 package com.goms.main.component
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -9,15 +8,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -153,11 +148,6 @@ fun MainOutingCard(
         if (getOutingCountUiState is GetOutingCountUiState.Success) {
             when (getOutingListUiState) {
                 GetOutingListUiState.Loading -> {
-                    Box(
-                        modifier = Modifier
-                            .size(56.dp, 23.dp)
-                            .shimmerEffect(color = colors.WHITE)
-                    )
                     LazyColumn(
                         modifier = Modifier
                             .fillMaxWidth()
