dcd258efb9bbf32b90d44dba7850c4195644c9fe
송현서 <s22026@gsm.hs.kr>
2024-03-26 17:36:41 +0900

    :sparkles: :: Detach dto and model to late

--- core/domain/src/main/java/com/goms/domain/council/GetLateListUseCase.kt
+++ core/domain/src/main/java/com/goms/domain/council/GetLateListUseCase.kt
@@ -1,7 +1,7 @@
 package com.goms.domain.council
 
 import com.goms.data.repository.council.CouncilRepository
-import com.goms.model.response.council.LateResponse
+import com.goms.model.response.council.LateResponseModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.datetime.LocalDate
 import javax.inject.Inject
@@ -9,6 +9,6 @@ import javax.inject.Inject
 class GetLateListUseCase @Inject constructor(
     private val councilRepository: CouncilRepository
 ) {
-    suspend operator fun invoke(date: LocalDate): Flow<List<LateResponse>> =
+    suspend operator fun invoke(date: LocalDate): Flow<List<LateResponseModel>> =
         councilRepository.getLateList(date = date)
 }
--- feature/main/src/main/java/com/goms/main/viewmodel/GetLateRankListUiState.kt
+++ feature/main/src/main/java/com/goms/main/viewmodel/GetLateRankListUiState.kt
@@ -1,10 +1,10 @@
 package com.goms.main.viewmodel
 
-import com.goms.model.response.late.RankResponse
+import com.goms.model.response.late.RankResponseModel
 
 sealed interface GetLateRankListUiState {
     object Loading : GetLateRankListUiState
     object Empty : GetLateRankListUiState
-    data class Success(val getLateRankListResponse: List<RankResponse>) : GetLateRankListUiState
+    data class Success(val getLateRankListResponse: List<RankResponseModel>) : GetLateRankListUiState
     data class Error(val exception: Throwable) : GetLateRankListUiState
 }
