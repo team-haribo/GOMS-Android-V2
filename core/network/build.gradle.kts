0df2847e4c132137fdf983d13b9bd61ae0f7cd9c
송현서 <s22026@gsm.hs.kr>
2024-03-26 20:22:16 +0900

    :fire: :: Remove enter

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -26,13 +26,10 @@ dependencies {
 
     implementation(libs.kotlinx.datetime)
     implementation(libs.kotlinx.serialization.json)
-
     implementation(libs.okhttp.logging)
-    
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.kotlin.serialization)
     implementation(libs.retrofit.moshi.converter)
-
     implementation(libs.moshi)
     ksp(libs.retrofit.moshi.codegen)
 }
