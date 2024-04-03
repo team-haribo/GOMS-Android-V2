2c8a8a98e6c8b85b57228075495f15c51a8f58fe
송현서 <s22026@gsm.hs.kr>
2024-04-03 12:01:40 +0900

    :heavy_plus_sign: :: Add in app update dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,4 +37,5 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.messaging)
+    implementation(libs.`in`.app.update)
 }
