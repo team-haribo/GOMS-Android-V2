db88483fd9bf82aa93e7c8c02f409251881d24ee
GSM-Minseok <ms0818000@gmail.com>
2024-04-05 00:32:30 +0900

    :memo: :: Add UpdateProfileImageUseCase.kt

---
+++ core/domain/src/main/java/com/goms/domain/account/UpdateProfileImageUseCase.kt
@@ -0,0 +1,13 @@
+package com.goms.domain.account
+
+import com.goms.data.repository.account.AccountRepository
+import okhttp3.MultipartBody
+import javax.inject.Inject
+
+class UpdateProfileImageUseCase @Inject constructor(
+    private val accountRepository: AccountRepository
+) {
+    suspend operator fun invoke(file: MultipartBody.Part) = kotlin.runCatching {
+        accountRepository.updateProfileImage(file)
+    }
+}
