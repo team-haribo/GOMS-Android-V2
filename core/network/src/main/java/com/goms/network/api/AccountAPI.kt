f6140130e5c3c9b0331dc9bf011d3cc4ec23cf8b
GSM-Minseok <ms0818000@gmail.com>
2024-04-05 00:31:44 +0900

    :memo: :: Add update image and delete image api

--- core/network/src/main/java/com/goms/network/api/AccountAPI.kt
+++ core/network/src/main/java/com/goms/network/api/AccountAPI.kt
@@ -4,6 +4,7 @@ import com.goms.network.dto.request.account.RePasswordRequest
 import com.goms.network.dto.response.account.ProfileResponse
 import okhttp3.MultipartBody
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.PATCH
@@ -15,10 +16,19 @@ interface AccountAPI {
     suspend fun getProfile(): ProfileResponse
 
     @Multipart
-    @POST("/api/v2/account/image")
-    suspend fun uploadProfileImage(
+    @PATCH("/api/v2/account/image")
+    suspend fun updateProfileImage(
         @Part file: MultipartBody.Part
     )
+
+    @Multipart
+    @POST("/api/v2/account/image")
+    suspend fun setProfileImage(
+        @Part file: MultipartBody.Part
+    )
+
+    @DELETE
+    suspend fun deleteProfileImage()
 
     @PATCH("/api/v2/account/new-password")
     suspend fun rePassword(
