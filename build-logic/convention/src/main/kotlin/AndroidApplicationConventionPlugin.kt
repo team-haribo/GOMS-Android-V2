48873f8ef3bf2870373eecb5a1ea43772385abf3
GSM-Minseok <ms0818000@gmail.com>
2024-04-06 10:46:44 +0900

    :sparkles: :: Cd test

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -22,7 +22,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                     minSdk = 26
                     targetSdk = 34
                     versionCode = 2
-                    versionName = "1.0.1"
+                    versionName = "1.0.2"
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
                     vectorDrawables.useSupportLibrary = true
