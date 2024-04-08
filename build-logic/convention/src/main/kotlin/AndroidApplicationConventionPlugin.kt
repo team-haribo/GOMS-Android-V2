dd3d10fbcb758054a3213c091b924df5c5c25df6
GSM-Minseok <ms0818000@gmail.com>
2024-04-08 10:48:21 +0900

    :sparkles: :: Change beta -> production

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -21,7 +21,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                     applicationId = "com.goms.goms_android_v2"
                     minSdk = 26
                     targetSdk = 34
-                    versionCode = 3
+                    versionCode = 4
                     versionName = "1.0.1"
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
