52917f00f3cdf13bceb5bdbb907853472103bf2d
송현서 <s22026@gsm.hs.kr>
2024-04-16 15:51:20 +0900

    :fire: :: Remove ui state

--- feature/re-password/src/main/java/com/goms/re_password/viewmodel/uistate/SendNumberUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.goms.re_password.viewmodel.uistate
-
-sealed interface SendNumberUiState {
-    object Loading : SendNumberUiState
-    object Success : SendNumberUiState
-    data class Error(val exception: Throwable): SendNumberUiState
-}
--- feature/re-password/src/main/java/com/goms/re_password/viewmodel/uistate/VerifyNumberUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.goms.re_password.viewmodel.uistate
-
-sealed interface VerifyNumberUiState {
-    object Loading : VerifyNumberUiState
-    object Success : VerifyNumberUiState
-    object BadRequest : VerifyNumberUiState
-    object NotFound : VerifyNumberUiState
-    data class Error(val exception: Throwable): VerifyNumberUiState
-}
