2e507dc295d44e0b104272eca8e8a05bac6afb5b
송현서 <s22026@gsm.hs.kr>
2024-04-16 15:51:03 +0900

    :sparkles: :: Add re password to RePasswordViewmodel

--- feature/re-password/src/main/java/com/goms/re_password/viewmodel/RePasswordViewmodel.kt
+++ feature/re-password/src/main/java/com/goms/re_password/viewmodel/RePasswordViewmodel.kt
@@ -2,16 +2,52 @@ package com.goms.re_password.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.goms.common.network.errorHandling
+import com.goms.domain.account.RePasswordUseCase
+import com.goms.model.request.account.RePasswordRequestModel
+import com.goms.re_password.viewmodel.uistate.RePasswordUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RePasswordViewmodel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
+    private val rePasswordUseCase: RePasswordUseCase
 ) : ViewModel() {
+    private val _rePasswordUiState = MutableStateFlow<RePasswordUiState>(RePasswordUiState.Loading)
+    val rePasswordUiState = _rePasswordUiState.asStateFlow()
+
     var password = savedStateHandle.getStateFlow(key = PASSWORD, initialValue = "")
     var newPassword = savedStateHandle.getStateFlow(key = NEW_PASSWORD, initialValue = "")
     var newCheckPassword = savedStateHandle.getStateFlow(key = NEW_CHECK_PASSWORD, initialValue = "")
+
+    fun rePassword(body: RePasswordRequestModel) = viewModelScope.launch {
+        rePasswordUseCase(body = body)
+            .onSuccess {
+                it.catch {  remoteError ->
+                    _rePasswordUiState.value = RePasswordUiState.Error(remoteError)
+                    remoteError.errorHandling(
+                        badRequestAction = { _rePasswordUiState.value = RePasswordUiState.BadRequest },
+                    )
+                }.collect { result ->
+                    _rePasswordUiState.value = RePasswordUiState.Success
+                }
+            }.onFailure {
+                _rePasswordUiState.value = RePasswordUiState.Error(it)
+                it.errorHandling(
+                    badRequestAction = { _rePasswordUiState.value = RePasswordUiState.BadRequest },
+                )
+            }
+    }
+
+    fun initRePassword() {
+        _rePasswordUiState.value = RePasswordUiState.Loading
+    }
 
     fun onPasswordChange(value: String) {
         savedStateHandle[PASSWORD] = value
