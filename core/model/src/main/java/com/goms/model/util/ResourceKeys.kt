d13c6e1b0a8af4538dd3aeadbd87c55d9de85182
송현서 <s22026@gsm.hs.kr>
2024-04-17 09:11:45 +0900

    :sparkles: :: Add Resource key device

--- core/model/src/main/java/com/goms/model/util/ResourceKeys.kt
+++ core/model/src/main/java/com/goms/model/util/ResourceKeys.kt
@@ -3,4 +3,6 @@ package com.goms.model.util
 object ResourceKeys {
     const val BEARER = "Bearer"
     const val EMAIL_DOMAIN = "@gsm.hs.kr"
+    const val DEVICE = "device"
+    const val DEVICE_TOKEN = "deviceToken"
 }
