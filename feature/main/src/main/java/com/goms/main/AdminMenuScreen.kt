2cea5d3015e479c4280a005f14b0c7048dc1f8e6
송현서 <s22026@gsm.hs.kr>
2024-04-18 15:04:09 +0900

    :lipstick: :: Add AdminMenuScreen

---
+++ feature/main/src/main/java/com/goms/main/AdminMenuScreen.kt
@@ -0,0 +1,76 @@
+package com.goms.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.goms.design_system.theme.GomsTheme
+import com.goms.main.component.AdminMenuList
+import com.goms.main.component.AdminMenuText
+import com.goms.model.enum.Authority
+import com.goms.ui.GomsRoleBackButton
+
+@Composable
+fun AdminMenuRoute(
+    onBackClick: () -> Unit,
+    onQrCreateClick: () -> Unit,
+    onStudentManagementClick: () -> Unit,
+    onOutingStatusClick: () -> Unit,
+    onLateListClick: () -> Unit,
+    onSettingClick: () -> Unit
+) {
+    AdminMenuScreen(
+        onBackClick = onBackClick,
+        onQrCreateClick = onQrCreateClick,
+        onStudentManagementClick = onStudentManagementClick,
+        onOutingStatusClick = onOutingStatusClick,
+        onLateListClick = onLateListClick,
+        onSettingClick = onSettingClick
+    )
+}
+
+@Composable
+fun AdminMenuScreen(
+    role: Authority = Authority.ROLE_STUDENT_COUNCIL,
+    onBackClick: () -> Unit,
+    onQrCreateClick: () -> Unit,
+    onStudentManagementClick: () -> Unit,
+    onOutingStatusClick: () -> Unit,
+    onLateListClick: () -> Unit,
+    onSettingClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(GomsTheme.colors.BACKGROUND)
+            .statusBarsPadding()
+            .navigationBarsPadding()
+    ) {
+        GomsRoleBackButton(role = role) {
+            onBackClick()
+        }
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AdminMenuText(modifier = Modifier.align(Alignment.Start))
+            Spacer(modifier = Modifier.height(16.dp))
+            AdminMenuList(
+                onQrCreateClick = onQrCreateClick,
+                onStudentManagementClick = onStudentManagementClick,
+                onOutingStatusClick = onOutingStatusClick,
+                onLateListClick = onLateListClick,
+                onSettingClick = onSettingClick
+            )
+        }
+    }
+}
