e1caa48660960ba5613382e40c0829c708c4495c
GSM-Minseok <ms0818000@gmail.com>
2024-05-09 15:16:26 +0900

    :recycle: :: Changing the location of the scanner declaration

--- feature/qrcode/src/main/java/com/goms/qrcode/util/QrcodeScanner.kt
+++ feature/qrcode/src/main/java/com/goms/qrcode/util/QrcodeScanner.kt
@@ -11,13 +11,14 @@ import com.google.mlkit.vision.common.InputImage
 class QrcodeScanner(
     val qrcodeData: (String?) -> Unit
 ) : ImageAnalysis.Analyzer {
-    @androidx.annotation.OptIn(androidx.camera.core.ExperimentalGetImage::class)
-    override fun analyze(imageProxy: ImageProxy) {
-        val options = BarcodeScannerOptions.Builder()
+
+    private val scanner = BarcodeScanning.getClient(
+        BarcodeScannerOptions.Builder()
             .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
             .build()
-
-        val scanner = BarcodeScanning.getClient(options)
+    )
+    @androidx.annotation.OptIn(androidx.camera.core.ExperimentalGetImage::class)
+    override fun analyze(imageProxy: ImageProxy) {
         val mediaImage = imageProxy.image
         mediaImage?.let {
             val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
