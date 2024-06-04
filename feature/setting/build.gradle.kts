d4f30ae2a72493a9fdd5a08aed28b6055b7183e1
송현서 <s22026@gsm.hs.kr>
2024-06-04 15:17:14 +0900

    :heavy_plus_sign: :: Add accompanist permission dependency

--- feature/setting/build.gradle.kts
+++ feature/setting/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("goms.android.feature")
     id("goms.android.hilt")
 }
+
 android {
     namespace = "com.goms.setting"
+}
+
+dependencies {
+    implementation(libs.accompanist.permission)
 }
