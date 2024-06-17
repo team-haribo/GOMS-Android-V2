6de185457e169e22a39c0bc0a020631d8c123f49
송현서 <s22026@gsm.hs.kr>
2024-06-17 20:37:57 +0900

    :sparkles: :: Apply GomsSpacer to find login module

--- feature/login/src/main/java/com/goms/login/component/LoginText.kt
+++ feature/login/src/main/java/com/goms/login/component/LoginText.kt
@@ -1,5 +1,6 @@
 package com.goms.login.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +13,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.goms.design_system.component.spacer.GomsSpacer
+import com.goms.design_system.component.spacer.SpacerSize
 import com.goms.design_system.theme.GomsTheme.colors
 import com.goms.design_system.theme.GomsTheme.typography
 import com.goms.login.R
@@ -19,14 +22,13 @@ import com.goms.login.R
 @Composable
 internal fun LoginText() {
     Column {
-        Row {
+        Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
             Text(
                 text = stringResource(id = R.string.wednesday_out_system),
                 style = typography.titleSmall,
                 fontWeight = FontWeight.SemiBold,
                 color = colors.P5
             )
-            Spacer(modifier = Modifier.width(4.dp))
             Text(
                 text = stringResource(id = R.string.management_service),
                 style = typography.titleSmall,
@@ -34,7 +36,7 @@ internal fun LoginText() {
                 color = colors.WHITE
             )
         }
-        Spacer(modifier = Modifier.height(8.dp))
+        GomsSpacer(size = SpacerSize.ExtraSmall)
         Text(
             text = stringResource(id = R.string.wednesday_out_easy_try),
             style = typography.textMedium,
