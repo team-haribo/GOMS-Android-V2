ca8974e7afb736e8878294446faeaa751c0d4d0a
송현서 <s22026@gsm.hs.kr>
2024-06-19 11:13:40 +0900

    :lipstick: :: Add preview to shimmer

--- core/design-system/src/main/java/com/goms/design_system/component/shimmer/shimmerEffect.kt
+++ core/design-system/src/main/java/com/goms/design_system/component/shimmer/shimmerEffect.kt
@@ -1,11 +1,18 @@
 package com.goms.design_system.component.shimmer
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.goms.design_system.component.indicator.GomsCircularProgressIndicator
+import com.goms.design_system.util.gomsPreview
 
 fun Modifier.shimmerEffect(
     color: Color = Color.LightGray,
@@ -17,4 +24,16 @@ fun Modifier.shimmerEffect(
         targetValue = targetValue
     )
     this.then(background(brush, shape))
+}
+
+@Preview
+@Composable
+fun GomsCircularProgressIndicatorPreview() {
+    gomsPreview {
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .shimmerEffect()
+        )
+    }
 }
