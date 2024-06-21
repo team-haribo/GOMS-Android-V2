d520af836cf4c7e9df26fcf30d188be70f851c80
송현서 <s22026@gsm.hs.kr>
2024-06-21 14:45:50 +0900

    :sparkles: :: Add TooManyRequestException to ui state

--- feature/sign-up/src/main/java/com/goms/sign_up/viewmodel/uistate/SendNumberUiState.kt
+++ feature/sign-up/src/main/java/com/goms/sign_up/viewmodel/uistate/SendNumberUiState.kt
@@ -4,5 +4,6 @@ sealed interface SendNumberUiState {
     object Loading : SendNumberUiState
     object Success : SendNumberUiState
     object EmailNotValid : SendNumberUiState
+    object TooManyRequest : SendNumberUiState
     data class Error(val exception: Throwable) : SendNumberUiState
 }
