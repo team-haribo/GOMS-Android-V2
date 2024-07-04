77779fb8a0dd4ccacbe1c77994a65d5b132216e8
GSM-Minseok <ms0818000@gmail.com>
2024-07-04 09:25:05 +0900

    :sparkles: :: Change context current LifecycleOwner

--- feature/qrcode/src/main/java/com/goms/qrcode/component/QrcodeScanPreview.kt
+++ feature/qrcode/src/main/java/com/goms/qrcode/component/QrcodeScanPreview.kt
@@ -1,6 +1,5 @@
 package com.goms.qrcode.component
 
-import android.content.Context
 import android.util.Log
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
@@ -15,9 +14,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.app.ComponentActivity
 import androidx.core.content.ContextCompat
-import com.goms.design_system.theme.GomsTheme
+import androidx.lifecycle.LifecycleOwner
 import com.goms.qrcode.util.QrcodeScanner
 import java.util.concurrent.Executors
 
@@ -66,7 +64,7 @@ internal fun QrcodeScanPreview(
                         cameraProvider.unbindAll()
 
                         cameraProvider.bindToLifecycle(
-                            context as ComponentActivity,
+                            context as LifecycleOwner,
                             cameraSelector,
                             preview,
                             imageCapture,
