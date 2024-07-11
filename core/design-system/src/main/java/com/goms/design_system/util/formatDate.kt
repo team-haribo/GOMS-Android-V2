631c9581580daf274fc799621a661f525c4bb7b2
송현서 <s22026@gsm.hs.kr>
2024-07-11 15:15:23 +0900

    :sparkles: :: Change type string -> localdate

--- core/design-system/src/main/java/com/goms/design_system/util/formatDate.kt
+++ core/design-system/src/main/java/com/goms/design_system/util/formatDate.kt
@@ -4,14 +4,12 @@ import kotlinx.datetime.LocalDate
 import java.time.format.TextStyle
 import java.util.Locale
 
-fun String.formatDate(): String {
-    val date = LocalDate.parse(this)
+fun LocalDate.formatDate(): String {
+    val year = this.year
+    val month = this.monthNumber
+    val day = this.dayOfMonth
 
-    val year = date.year
-    val month = date.monthNumber
-    val day = date.dayOfMonth
-
-    val dayOfWeek = date.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN)
+    val dayOfWeek = this.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN)
 
     return "${year}년 ${month}월 ${day}일 ($dayOfWeek)"
 }
