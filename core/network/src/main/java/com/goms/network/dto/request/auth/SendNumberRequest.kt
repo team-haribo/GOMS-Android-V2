cd7eb2cb69ceb36f0aa20d2b9e203c05e4596433
송현서 <s22026@gsm.hs.kr>
2024-07-12 10:09:03 +0900

    :sparkles: :: Add emailStatus to model and dto

--- core/network/src/main/java/com/goms/network/dto/request/auth/SendNumberRequest.kt
+++ core/network/src/main/java/com/goms/network/dto/request/auth/SendNumberRequest.kt
@@ -1,9 +1,11 @@
 package com.goms.network.dto.request.auth
 
+import com.goms.model.enum.EmailStatus
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class SendNumberRequest(
-    @Json(name = "email") val email: String
+    @Json(name = "email") val email: String,
+    @Json(name = "emailStatus") val emailStatus: EmailStatus,
 )
