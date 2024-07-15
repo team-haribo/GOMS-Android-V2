3cedcc9dc049d2464d39d21e8d0bd378648c20e2
송현서 <s22026@gsm.hs.kr>
2024-07-15 21:03:53 +0900

    :rocket: :: Update version 1.2.8

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -21,8 +21,8 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                     applicationId = "com.goms.goms_android_v2"
                     minSdk = 26
                     targetSdk = 34
-                    versionCode = 18
-                    versionName = "1.2.7"
+                    versionCode = 19
+                    versionName = "1.2.8"
                     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
                     vectorDrawables.useSupportLibrary = true
