30c5f2a04195faa34f6ab04b13c5e347d1debc72
GSM-Minseok <ms0818000@gmail.com>
2024-08-12 15:06:27 +0900

    :sparkles: :: Remove unused import in feature-re_password

--- feature/re-password/src/main/java/com/goms/re_password/PasswordCheckScreen.kt
+++ feature/re-password/src/main/java/com/goms/re_password/PasswordCheckScreen.kt
@@ -1,7 +1,6 @@
 package com.goms.re_password
 
 import android.content.pm.ActivityInfo
-import android.content.res.Configuration
 import androidx.activity.ComponentActivity
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
@@ -26,7 +24,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
