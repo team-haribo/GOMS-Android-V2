360d2bebedf6aec79340d26add2eb834d42fdecb
GSM-Minseok <ms0818000@gmail.com>
2024-08-12 11:59:03 +0900

    :sparkles: :: Remove unused import in app

--- app/src/main/java/com/goms/goms_android_v2/GomsNotification.kt
+++ app/src/main/java/com/goms/goms_android_v2/GomsNotification.kt
@@ -6,10 +6,10 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
 import androidx.core.app.NotificationCompat
-import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
 import com.goms.design_system.R
 import com.goms.domain.notification.SaveDeviceTokenUseCase
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
