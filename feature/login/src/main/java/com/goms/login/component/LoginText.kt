0b5ab9046f84b39f1f6fc24e32496d1c5b8590ae
GSM-Minseok <ms0818000@gmail.com>
2024-08-12 14:59:51 +0900

    :sparkles: :: Remove unused import in feature-login-component

--- feature/login/src/main/java/com/goms/login/component/LoginText.kt
+++ feature/login/src/main/java/com/goms/login/component/LoginText.kt
@@ -3,12 +3,8 @@ package com.goms.login.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
