383e73f3f0024183b1c4edb48454bce258c4d9b2
GSM-Minseok <ms0818000@gmail.com>
2024-08-12 12:02:14 +0900

    :sparkles: :: Remove unused import in core-data-outing

--- core/data/src/main/java/com/goms/data/repository/outing/OutingRepository.kt
+++ core/data/src/main/java/com/goms/data/repository/outing/OutingRepository.kt
@@ -2,8 +2,6 @@ package com.goms.data.repository.outing
 
 import com.goms.model.response.outing.CountResponseModel
 import com.goms.model.response.outing.OutingResponseModel
-import com.goms.network.dto.response.outing.CountResponse
-import com.goms.network.dto.response.outing.OutingResponse
 import kotlinx.coroutines.flow.Flow
 import java.util.UUID
 
