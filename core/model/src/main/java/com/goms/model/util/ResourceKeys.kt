bcbb1134d0e7d9cbf7c650a62c1948c13d17abb5
GSM-Minseok <ms0818000@gmail.com>
2024-08-12 12:15:23 +0900

    :sparkles: :: Remove unused resource in core-model-util

--- core/model/src/main/java/com/goms/model/util/ResourceKeys.kt
+++ core/model/src/main/java/com/goms/model/util/ResourceKeys.kt
@@ -3,7 +3,5 @@ package com.goms.model.util
 object ResourceKeys {
     const val BEARER = "Bearer"
     const val EMAIL_DOMAIN = "@gsm.hs.kr"
-    const val DEVICE = "device"
-    const val DEVICE_TOKEN = "deviceToken"
     const val EMPTY = ""
 }
