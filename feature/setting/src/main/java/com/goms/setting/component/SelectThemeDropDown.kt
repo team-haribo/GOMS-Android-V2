2ec9c9b0ff42f48db131233e6a5974a45de8d4f8
GSM-Minseok <ms0818000@gmail.com>
2024-08-12 15:06:59 +0900

    :sparkles: :: Remove unused import in feature-setting-component

--- feature/setting/src/main/java/com/goms/setting/component/SelectThemeDropDown.kt
+++ feature/setting/src/main/java/com/goms/setting/component/SelectThemeDropDown.kt
@@ -1,9 +1,7 @@
 package com.goms.setting.component
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
--- feature/setting/src/main/java/com/goms/setting/component/SettingButton.kt
+++ feature/setting/src/main/java/com/goms/setting/component/SettingButton.kt
@@ -3,11 +3,8 @@ package com.goms.setting.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
