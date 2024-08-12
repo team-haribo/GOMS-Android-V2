fbf130d09c5dd8f6ce3f4c70b7f0a4092ce60d50
GSM-Minseok <ms0818000@gmail.com>
2024-08-12 12:07:52 +0900

    :sparkles: :: Remove unused import in core-design_system-component-textfield

--- core/design-system/src/main/java/com/goms/design_system/component/textfield/GomsTextField.kt
+++ core/design-system/src/main/java/com/goms/design_system/component/textfield/GomsTextField.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
