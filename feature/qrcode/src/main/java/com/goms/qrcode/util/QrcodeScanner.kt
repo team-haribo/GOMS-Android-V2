03f55b6f3dc7f563606c00747d82fac6c915f6fd
GSM-Minseok <ms0818000@gmail.com>
2024-11-01 11:22:11 +0900

    :sparkles: :: Change mediaImage type

--- feature/qrcode/src/main/java/com/goms/qrcode/util/QrcodeScanner.kt
+++ feature/qrcode/src/main/java/com/goms/qrcode/util/QrcodeScanner.kt
@@ -5,6 +5,7 @@ import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.ZoomSuggestionOptions
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 
@@ -25,9 +26,9 @@ class QrcodeScanner(
             return
         }
 
-        val mediaImage = imageProxy.image
+        val mediaImage = imageProxy.toBitmap()
         mediaImage?.let {
-            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+            val image = InputImage.fromBitmap(mediaImage, imageProxy.imageInfo.rotationDegrees)
 
             scanner.process(image)
                 .addOnSuccessListener { barcodes ->
