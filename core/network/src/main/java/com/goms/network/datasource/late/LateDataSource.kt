f26f5eaa12561e38c499cd9c92b80d8f98acd2da
kimjeakwan <s24042@gsm.hs.kr>
2025-02-09 16:02:29 +0900

    :recycle: :: suspend 삭제

--- core/network/src/main/java/com/goms/network/datasource/late/LateDataSource.kt
+++ core/network/src/main/java/com/goms/network/datasource/late/LateDataSource.kt
@@ -4,5 +4,5 @@ import com.goms.network.dto.response.late.RankResponse
 import kotlinx.coroutines.flow.Flow
 
 interface LateDataSource {
-    suspend fun getLateRankList(): Flow<List<RankResponse>>
+    fun getLateRankList(): Flow<List<RankResponse>>
 }
