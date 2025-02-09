b691f320cc9e6cf366c8bf21252ef346f6ffb567
kimjeakwan <s24042@gsm.hs.kr>
2025-02-09 16:01:51 +0900

    :recycle: :: suspend 삭제

--- core/domain/src/main/java/com/goms/domain/council/ForcingOutingUseCase.kt
+++ core/domain/src/main/java/com/goms/domain/council/ForcingOutingUseCase.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 class ForcingOutingUseCase @Inject constructor(
     private val councilRepository: CouncilRepository
 ) {
-    suspend operator fun invoke(outingIdx: UUID) = kotlin.runCatching {
+    operator fun invoke(outingIdx: UUID) = kotlin.runCatching {
         councilRepository.forcingOuting(outingIdx = outingIdx)
     }
 }
