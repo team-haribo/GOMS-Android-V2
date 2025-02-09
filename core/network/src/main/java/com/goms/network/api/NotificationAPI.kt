2fda8cddbd5a97bc42953ca98896d1c98d584409
kimjeakwan <s24042@gsm.hs.kr>
2025-02-09 16:03:28 +0900

    :recycle: ::  suspend 삭제

--- core/network/src/main/java/com/goms/network/api/NotificationAPI.kt
+++ core/network/src/main/java/com/goms/network/api/NotificationAPI.kt
@@ -2,15 +2,16 @@ package com.goms.network.api
 
 import com.goms.network.di.RequestUrls
 import retrofit2.http.DELETE
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface NotificationAPI {
     @POST(RequestUrls.NOTIFICATION.saveToken)
-    suspend fun saveDeviceToken(
-        @Path("deviceToken") deviceToken: String
+    fun saveDeviceToken(
+        @Path("deviceToken") deviceToken: String,
     )
 
     @DELETE(RequestUrls.NOTIFICATION.deleteToken)
-    suspend fun deleteDeviceToken()
+    fun deleteDeviceToken()
 }
