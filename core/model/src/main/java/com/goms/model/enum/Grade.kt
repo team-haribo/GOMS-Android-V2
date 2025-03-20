0bc54cfc910bd67c240f52ffaa9da30f9f44db6a
jsw9436 <jswjsw101010@gmail.com>
2025-03-20 13:43:08 +0900

    :art: :: 학년별 기수 변경

--- core/model/src/main/java/com/goms/model/enum/Grade.kt
+++ core/model/src/main/java/com/goms/model/enum/Grade.kt
@@ -1,7 +1,7 @@
 package com.goms.model.enum
 
 enum class Grade(val value: String, val enum: Int) {
-    FIRST_GRADE("1학년", 8),
-    SECOND_GRADE("2학년", 7),
-    THIRD_GRADE("3학년", 6)
+    FIRST_GRADE("1학년", 9),
+    SECOND_GRADE("2학년", 8),
+    THIRD_GRADE("3학년", 7)
 }
